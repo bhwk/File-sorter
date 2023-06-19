e5e5048d2b58b2e92967305626532dcb87e62885
bhwk <120708160+bhwk@users.noreply.github.com>
2023-06-20 00:39:38 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,109 @@
+use anyhow::{anyhow, Result};
+use infer;
+use std::{env, fs, path};
+
+fn setup_folders() -> Result<()> {
+    let folders = vec![
+        "Image",
+        "Video",
+        "Audio",
+        "Archive",
+        "Book",
+        "Documents",
+        "Font",
+        "Application",
+    ];
+    let working_dir = env::current_dir()?;
+    if let Some(name) = working_dir.file_name() {
+        if let Some(filename_str) = name.to_str() {
+            if folders.contains(&filename_str) {
+                return Ok(());
+            }
+        }
+    }
+    for path in folders {
+        let path = path::Path::new(path);
+        if path.exists() {
+            continue;
+        } else {
+            fs::create_dir(path)?
+        }
+    }
+    Ok(())
+}
+
+fn check_type(path: &path::PathBuf) -> Result<String> {
+    let path_read = &fs::read(&path)?;
+    if infer::is_image(path_read) {
+        return Ok(String::from("Image"));
+    }
+
+    if infer::is_video(path_read) {
+        return Ok(String::from("Video"));
+    }
+
+    if infer::is_audio(path_read) {
+        return Ok(String::from("Audio"));
+    }
+
+    if infer::is_archive(path_read) {
+        return Ok(String::from("Archive"));
+    }
+
+    if infer::is_book(path_read) {
+        return Ok(String::from("Book"));
+    }
+
+    if infer::is_document(path_read) {
+        return Ok(String::from("Document"));
+    }
+
+    if infer::is_font(path_read) {
+        return Ok(String::from("Font"));
+    }
+
+    if infer::is_app(path_read) {
+        return Ok(String::from("Application"));
+    }
+
+    return Err(anyhow!("File is not a known type"));
+}
+
+fn move_file(path: path::PathBuf) -> Result<()> {
+    let file_type: String = check_type(&path)?;
+
+    let mut new_path = path::PathBuf::from(&file_type);
+    new_path.push(&path.file_name().unwrap().to_str().unwrap());
+    println!(
+        "Moving {} to {}",
+        &path.file_name().unwrap().to_str().unwrap(),
+        &file_type
+    );
+    fs::rename(path, new_path)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let _ = setup_folders();
+
+    let working_dir = env::current_dir()?;
+
+    for entry in fs::read_dir(&working_dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_dir() {
+            continue;
+        }
+        let file_type = infer::get_from_path(&path)?;
+
+        match file_type {
+            Some(_) => {
+                move_file(path)?;
+            }
+            None => continue,
+        }
+    }
+
+    Ok(())
+}
